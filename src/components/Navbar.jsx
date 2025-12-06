65dbe23cf6c3a6b132edd75b739e2b8e9734b403
Shawn Kebel <fxrender@outlook.com>
2025-12-06 00:16:32 -0600

    :sparkles: Add `Navbar` component with dynamic navigation links and icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+
+import { navIcons, navLinks } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Shawn's Portfolio</p>
+        <ul className="">
+          {navLinks.map(({ id, name }) => (
+            <li key={id} className="">
+              <p className="">{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id} className="">
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time datetime="">{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
