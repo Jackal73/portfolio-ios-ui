ccf5a54a5ed59c09813f2b7326c566cbf59824b1
Shawn Kebel <fxrender@outlook.com>
2025-12-10 21:48:33 -0600

    :sparkles: Add Resume window using `react-pdf` with `PDF viewer` and integrate into Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import dayjs from "dayjs";
 
 import { navIcons, navLinks } from "#constants";
+import useWindowStore from "#store/window.js";
 
 const Navbar = () => {
+  const { openWindow } = useWindowStore();
+
   return (
     <nav>
       <div>
         <img src="/images/logo.svg" alt="logo" />
         <p className="font-bold">Shawn's Portfolio</p>
         <ul className="">
-          {navLinks.map(({ id, name }) => (
-            <li key={id} className="">
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)} className="">
               <p className="">{name}</p>
             </li>
           ))}
