6a3faab95309732b6d041985330ecd98db9881c8
Shawn Kebel <fxrender@outlook.com>
2025-12-10 22:06:51 -0600

    :building_construction: Add Zustand location store for `managing location state` with default location setup

---
+++ src/store/location.js
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { locations } from "#constants";
+
+const DEFAULT_LOCATION = locations.work;
+
+// const { create } = require("zustand");
+// const { immer } = require("zustand/middleware/immer");
+
+const useLocationStore = create(
+  immer((set) => ({
+    activeLocation: DEFAULT_LOCATION,
+    setActiveLocation: (location = null) =>
+      set((state) => {
+        state.activeLocation = location;
+      }),
+
+    resetActiveLocation: () =>
+      set((state) => {
+        state.activeLocation = DEFAULT_LOCATION;
+      }),
+  }))
+);
+
+export default useLocationStore;
